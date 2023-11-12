6bcba741c6d4ee076f820a6460751bd4fd906bde
JeronimoMorais <jeronimo.cm@outlook.com>
2023-11-11 22:22:35 -0300

    Ajuste no nome da classe e comparação de tipos.

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,8 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        
+        short numeroCurto = 1;
+        int numerNormal = numeroCurto;
+        //short numeroCurto2 = numerNormal; não é permitido este formato porque o tipo INT é maior do que o SHORT
+        short numeroCurto2 = (short) numerNormal; //ao fazer um CASTING a expressão fica correta.
     }
 }
