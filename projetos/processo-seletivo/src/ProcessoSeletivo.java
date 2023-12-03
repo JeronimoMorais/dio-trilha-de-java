19f9e8d4da6429e50d05a298d589945e3eda7e3a
JeronimoMorais <jeronimo.cm@outlook.com>
2023-12-03 16:44:05 -0300

    Parte 02 do projeto.

--- projetos/processo-seletivo/src/ProcessoSeletivo.java
+++ projetos/processo-seletivo/src/ProcessoSeletivo.java
@@ -1,8 +1,35 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
     public static void main(String[] args) throws Exception {
-        analisarCandidatura(1500.0);
-        analisarCandidatura(2000.0);
-        analisarCandidatura(2200.0);
+        // analisarCandidatura(1500.0);
+        // analisarCandidatura(2000.0);
+        // analisarCandidatura(2200.0);
+
+        selecaoCandidatos();
+    }
+
+    static void selecaoCandidatos(){
+        String[] candidatos = {"JERONIMO", "IZABEL", "ADRIANO", "RAFAEL", "PAOLA", "CECILIA", "LUCAS", "MARTA", "ANTONIO", "BRISA"};
+
+        int candidatosSelecionados = 0;
+        int candidatoAtual = 0;
+        double salarioOfertado = 2000.0;
+        while (candidatosSelecionados < 5 && candidatoAtual < candidatos.length) {
+                String candidato = candidatos[candidatoAtual];
+                double salarioRequerido = valorDesejado();
+
+                System.out.printf("O candidato " + candidato + " requereu o salário R$ %.2f.\n", salarioRequerido);
+                if(salarioOfertado >= salarioRequerido){
+                    System.out.println("O candidato " + candidato + " foi selecionado!");
+                    candidatosSelecionados++;
+                }
+                candidatoAtual++;
+        }
+    }
+
+    static double valorDesejado(){
+        return ThreadLocalRandom.current().nextDouble(1800.0, 2200.0);
     }
 
     static void analisarCandidatura(double salarioDesejado){
