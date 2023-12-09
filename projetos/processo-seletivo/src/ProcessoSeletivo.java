a617c81da467dfa3432c89c426f2f7cebdad22e9
JeronimoMorais <jeronimo.cm@outlook.com>
2023-12-09 10:58:24 -0300

    Parte 04 do projeto.

--- projetos/processo-seletivo/src/ProcessoSeletivo.java
+++ projetos/processo-seletivo/src/ProcessoSeletivo.java
@@ -1,7 +1,14 @@
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) throws Exception {
+
+        String[] candidatoSelecionados = {"JERONIMO", "IZABEL", "ADRIANO", "RAFAEL", "PAOLA"};
+
+        for(String candidato:candidatoSelecionados){
+            ligarParaCandidato(candidato);
+        }
         
         // analisarCandidatura(1500.0);
         // analisarCandidatura(2000.0);
@@ -9,7 +16,32 @@ public class ProcessoSeletivo {
 
         // selecaoCandidatos();
 
-        imprimirCandidatosSelecionados();
+        // imprimirCandidatosSelecionados();
+    }
+
+    static void ligarParaCandidato(String candidato){
+        int tentativasRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+        do{
+            atendeu = atender();
+            continuarTentando = !atendeu;
+                if(continuarTentando){
+                    tentativasRealizadas ++;
+                }else{
+                    System.out.println("CONTATO REALIZADO COM SUCESSO!");
+                }
+        }while (continuarTentando && tentativasRealizadas<3);
+
+        if(atendeu){
+            System.out.println("CONSEGUIMOS CONTATO COM " + candidato + " NA " + tentativasRealizadas + "ª TENTATIVA.");
+        }else{
+            System.out.println("NÃO CONSEGUIMOS CONTATO COM " + candidato + ", NÚMERO MÁXIMO DE TENTATIVAS "+ tentativasRealizadas + " REALIZADAS.");
+        }
+    }
+
+    static boolean atender(){
+        return new Random().nextInt(3)==1;
     }
 
     static void imprimirCandidatosSelecionados(){
